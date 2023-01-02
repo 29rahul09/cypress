a41553e842e90415aa87e9d81435edb7fe82b93b
rahul <90131464+Rahul290987@users.noreply.github.com>
2023-01-02 17:16:29 +0530

    Page object model commit

---
+++ cypress/e2e/D-pom.cy.js
@@ -0,0 +1,33 @@
+import { loginPage } from "./POM/Page_login";
+const pageLogin = new loginPage
+
+before(() => {
+    cy.log('Test are ready to perform')
+})
+
+describe('POM Demo', () => {
+
+    beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
+    })
+
+    it('Login test 1 ', () => {
+
+        pageLogin.username('Admin')
+        pageLogin.password('admin123')
+        pageLogin.login()
+        cy.get('.oxd-input').type('orange')
+
+    });
+
+    it('Login test 2 ', () => {
+
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.get('.oxd-main-menu-search > .oxd-icon-button').click()
+
+    });
+
+
+})
