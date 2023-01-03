4934be56fc354d6692be982a07f3495e29fe96da
rahul <90131464+Rahul290987@users.noreply.github.com>
2023-01-03 18:39:38 +0530

    modify last commit

--- cypress/e2e/D-readWriteFile.cy.js
+++ cypress/e2e/D-readWriteFile.cy.js
@@ -14,9 +14,9 @@ describe('Read and Write Methods', () => {
     });
 
     it('Write file using writeFile()', () => {
-        cy.writeFile('./cypress/sample.txt', 'My name is Rahul\n')
-        cy.writeFile('./cypress/sample.txt', 'I live in Bareilly\n')
-        cy.writeFile('./cypress/sample.txt', 'I m learning cypress', { flag: 'a+' })
+        cy.writeFile('sample.txt', 'My name is Rahul\n')
+        cy.writeFile('sample.txt', 'I live in Bareilly\n', { flag: 'a+' })
+        cy.writeFile('sample.txt', 'I m learning cypress', { flag: 'a+' })
     });
 
 })
