c73c3a45e26a64f37ad58b7927c783611803849e
rahul <90131464+Rahul290987@users.noreply.github.com>
2023-01-12 12:15:42 +0530

    mocha commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,13 @@ const { defineConfig } = require("cypress");
 const {downloadFile} = require('cypress-downloadfile/lib/addPlugin');
 
 module.exports = defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',  // for html reports
   e2e: {
     watchForFileChanges : false,
     defaultCommandTimeout : 3000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on); // for html reports
       on('task', {downloadFile})
     },
   },
