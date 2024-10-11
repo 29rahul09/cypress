f4f28763f70e0c55d172b8f3b85029d981c32127
Rahul-BMJ <rsingh@bmj.com>
2024-10-11 17:18:12 +0530

    update code with promise

--- cypress/e2e/CSV-ArticelSmokeTest.cy.js
+++ cypress/e2e/CSV-ArticelSmokeTest.cy.js
@@ -8,12 +8,6 @@ describe(
     viewportWidth: 1280,
   },
   () => {
-    const brokenImages = [];
-    const nonBrokenImages = [];
-    const missingSupplementary = [];
-    const noSupplementary = [];
-    const missingRR = [];
-
     const validateArticlePage = (articleUrl) => {
       cy.visit({
         url: `${domain}${articleUrl}`,
@@ -22,31 +16,83 @@ describe(
 
       cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
 
-      validateTopBoxSection();
+      validateTopBoxSection(articleUrl);
       validateImages(articleUrl);
       validateSupplementaryMaterials(articleUrl);
       validateRapidResponses(articleUrl);
     };
+    const validateTopBoxSection = (articleUrl) => {
+      const topBoxError = [];
+      cy.get("body").then(($body) => {
+        const promises = []; // Array to store promises
 
-    const validateTopBoxSection = () => {
-      cy.get("h1#article-title-1").should("be.visible");
-      cy.get(
-        '[data-testid="request-permissions"] [data-testid="request-permissions-text"]'
-      )
-        .should("exist")
-        .contains("Request permission");
-      cy.get('[data-testid="citation"]')
-        .should("exist")
-        .contains("Cite this article");
-      cy.get('[data-testid="share"]').should("exist").contains("Share");
-      cy.get('[data-testid="pdf"] a')
-        .should("have.attr", "href")
-        .then((href) => {
-          cy.request(href).its("status").should("eq", 200);
+        if ($body.find("#article-title-1").length > 0) {
+          promises.push(cy.get("h1#article-title-1").should("be.visible"));
+        }
+
+        if (
+          $body.find(
+            '[data-testid="request-permissions"] [data-testid="request-permissions-text"]'
+          ).length > 0
+        ) {
+          promises.push(
+            cy
+              .get(
+                '[data-testid="request-permissions"] [data-testid="request-permissions-text"]'
+              )
+              .should("exist")
+              .contains("Request permission")
+          );
+        }
+
+        if ($body.find('[data-testid="citation"]').length > 0) {
+          promises.push(
+            cy
+              .get('[data-testid="citation"]')
+              .should("exist")
+              .contains("Cite this article")
+          );
+        }
+
+        if ($body.find('[data-testid="share"]').length > 0) {
+          promises.push(
+            cy.get('[data-testid="share"]').should("exist").contains("Share")
+          );
+        }
+
+        if ($body.find('[data-testid="pdf"] a').length > 0) {
+          promises.push(
+            cy
+              .get('[data-testid="pdf"] a')
+              .should("have.attr", "href")
+              .then((href) => {
+                return cy
+                  .request({ url: `${href}`, failOnStatusCode: false })
+                  .then((response) => {
+                    if (response.status !== 200) {
+                      topBoxError.push(`PDF link is missing ==> ${articleUrl}`);
+                    }
+                  });
+              })
+          );
+        }
+
+        // Wait for all promises to resolve
+        Promise.all(promises).then(() => {
+          if (topBoxError.length > 0) {
+            const topBoxCsvContent = "Top Box Error\n" + topBoxError.join("\n");
+            cy.writeFile(
+              `cypress/inspection/SmokeTest/${journal}/TopBoxError.csv`,
+              topBoxCsvContent
+            );
+          }
         });
+      });
     };
 
     const validateSupplementaryMaterials = (articleUrl) => {
+      const missingSupplementary = [];
+      const noSupplementary = [];
       cy.get("body").then(($body) => {
         if ($body.find("#supplementary-materials").length > 0) {
           cy.get("#supplementary-materials").click();
@@ -78,6 +124,7 @@ describe(
     };
 
     const validateRapidResponses = (articleUrl) => {
+      const missingRR = [];
       cy.get("body").then(($body) => {
         if ($body.find("#rapid-responses").length > 0) {
           cy.get("#rapid-responses").click();
@@ -97,6 +144,8 @@ describe(
     };
 
     const validateImages = (articleUrl) => {
+      const brokenImages = [];
+      const nonBrokenImages = [];
       cy.get("main")
         .find("img")
         .each(($img, i) => {
